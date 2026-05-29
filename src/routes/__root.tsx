e1c82313a91cab80c3603c601fc7ecbe0469620f
Royan Rosyad <royan.rosyad21@mhs.uinjkt.ac.id>
2026-05-29 10:42:03 +0700

    Update src/routes/__root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,21 @@
+import { lazy, Suspense } from 'react'
 import { type QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Toaster } from '@/components/ui/sonner'
 import { NavigationProgress } from '@/components/navigation-progress'
 import { GeneralError } from '@/features/errors/general-error'
 import { NotFoundError } from '@/features/errors/not-found-error'
+
+const ReactQueryDevtools = lazy(() =>
+  import('@tanstack/react-query-devtools').then(({ ReactQueryDevtools }) => ({
+    default: ReactQueryDevtools,
+  }))
+)
+const TanStackRouterDevtools = lazy(() =>
+  import('@tanstack/react-router-devtools').then(
+    ({ TanStackRouterDevtools }) => ({ default: TanStackRouterDevtools })
+  )
+)
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient
@@ -17,10 +27,10 @@ export const Route = createRootRouteWithContext<{
         <Outlet />
         <Toaster duration={5000} />
         {import.meta.env.MODE === 'development' && (
-          <>
+          <Suspense fallback={null}>
             <ReactQueryDevtools buttonPosition='bottom-left' />
             <TanStackRouterDevtools position='bottom-right' />
-          </>
+          </Suspense>
         )}
       </>
     )
