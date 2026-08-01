c35d7fcc54321a5a5ee7e347f6df8d0552c77dde
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2026-08-01 22:18:01 +0700

    refactor(ui): remove unused components

--- src/components/data-table/view-options.tsx
+++ src/components/data-table/view-options.tsx
@@ -32,24 +32,20 @@ export function DataTableViewOptions<TData>({
       <DropdownMenuContent align='end' className='w-[150px]'>
         <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        {table
-          .getAllColumns()
-          .filter(
-            (column) =>
-              typeof column.accessorFn !== 'undefined' && column.getCanHide()
-          )
-          .map((column) => {
-            return (
-              <DropdownMenuCheckboxItem
-                key={column.id}
-                className='capitalize'
-                checked={column.getIsVisible()}
-                onCheckedChange={(value) => column.toggleVisibility(!!value)}
-              >
-                {column.id}
-              </DropdownMenuCheckboxItem>
-            )
-          })}
+        {table.getAllColumns().flatMap((column) =>
+          typeof column.accessorFn !== 'undefined' && column.getCanHide()
+            ? [
+                <DropdownMenuCheckboxItem
+                  key={column.id}
+                  className='capitalize'
+                  checked={column.getIsVisible()}
+                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                >
+                  {column.id}
+                </DropdownMenuCheckboxItem>,
+              ]
+            : []
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   )
