8552afe684c1450ec24270c3e2c8af2dcd434470
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2026-08-25 14:12:56 +0700

    perf(context): memoize shared provider values

--- src/context/search-provider.tsx
+++ src/context/search-provider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { CommandMenu } from '@/components/command-menu'
 
 type SearchContextType = {
@@ -26,8 +26,10 @@ export function SearchProvider({ children }: SearchProviderProps) {
     return () => document.removeEventListener('keydown', down)
   }, [])
 
+  const contextValue = useMemo(() => ({ open, setOpen }), [open])
+
   return (
-    <SearchContext value={{ open, setOpen }}>
+    <SearchContext value={contextValue}>
       {children}
       <CommandMenu />
     </SearchContext>
